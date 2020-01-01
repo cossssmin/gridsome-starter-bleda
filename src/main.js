bc5be415793479bf36c526167e56c9d0f4fe6cce
Justin Yoo <justin.yoojh@gmail.com>
2020-01-01 22:34:04 +1100

    Update personalisation for ko

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
 
-  head.htmlAttrs = { lang: 'en', class: 'h-full' }
-  head.bodyAttrs = { class: 'antialiased font-serif' }
+  head.htmlAttrs = { lang: 'ko', class: 'h-full' }
+  head.bodyAttrs = { class: 'antialiased font-sans' }
 
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Fira+Sans:400,700%7CCardo'
+  })
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800&display=swap&subset=korean'
   })
   head.link.push({
     rel: 'stylesheet',
