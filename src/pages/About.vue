f0f7b7571bca37a0fc351b243bc3ed44984b716b
Justin Yoo <justin.yoojh@gmail.com>
2020-01-04 22:46:53 +1100

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -56,7 +56,7 @@ export default {
 
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: 'About' },
-        { name: "twitter:description", content: 'Introduction to Dev Kimchi' },
+        { name: "twitter:description", content: this.ogDescription },
         { name: "twitter:site", content: "@devkimchi" },
         { name: "twitter:creator", content: "@devkimchi" },
         { name: "twitter:image", content: this.ogImageUrl },
